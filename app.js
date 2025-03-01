2de66b1bfe5fd8be4ca06f7ec38c79bf2d2520a8
vmr093 <ranjha.viola@gmail.com>
2025-02-28 21:20:59 -0600

    practicing parallax

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const section = document.querySelector("section");
 const image_container = document.querySelector(".imgContainer");
 const opacity = document.querySelectorAll(".opacity");
 const border = document.querySelector(".border");
+const moreInfoBorder = document.querySelector(".more-info-border");
 
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
-    
+
     translate.forEach(element => {
         let speed = element.dataset.speed;
         element.style.transform = `translateY(${scroll * speed}px)`;
@@ -22,13 +23,16 @@ window.addEventListener('scroll', () => {
 
     opacity.forEach(element => {
         element.style.opacity = scroll / (sectionY.top + section_height);
-    })
+    });
 
-    big_title.style.opacity = - scroll / (header_height / 2) + 1;
+    big_title.style.opacity = -scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
     content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
     image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
 
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
-})
+
+    // Animate the "More Info" section border
+    moreInfoBorder.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
+});
